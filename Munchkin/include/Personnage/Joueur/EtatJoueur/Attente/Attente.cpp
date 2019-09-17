3e7f5123ecae183397100520a8bb00cd1e25e4fc
QuentinLeCorvec <50618902+QuentinLeCorvec@users.noreply.github.com>
2019-09-17 18:22:31 +0200

    Create Attente.cpp

---
+++ Munchkin/include/Personnage/Joueur/EtatJoueur/Attente/Attente.cpp
@@ -0,0 +1,13 @@
+/**
+ * \file Attente.cpp
+ * \brief implémentation classe Attente
+ * \author Le Corvec Quentin
+ * \date Octobre 2014
+ */
+
+
+#include "Attente.hpp"
+Attente::Attente(Joueur * j):EtatJoueur(j){}
+
+
+Attente::~Attente(){}
